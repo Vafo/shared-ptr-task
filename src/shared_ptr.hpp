46631fbb60c61f8174ef3dbae399e3a2e63fe4f7
vafo <v.safokhujaev@student.inha.uz>
2023-12-21 14:59:55 +0500

    shared_ptr details moved out to separate file
    Related to #1

--- src/shared_ptr.hpp
+++ src/shared_ptr.hpp
@@ -4,55 +4,10 @@
 #include <atomic>
 #include <cassert>
 
-#include "checked_delete.hpp"
+#include "shared_ptr_details.hpp"
 #include "scoped_ptr.hpp"
 
 namespace memory {
-
-// Forward Declaration
-template<
-    typename T,
-    typename Allocator
->
-class shared_ptr;
-
-namespace detail {
-
-template<
-    typename T,
-    typename Allocator = std::allocator<T>
->
-class shared_ptr_impl {
-public: /*public for allocator*/
-
-    typedef 
-        std::allocator_traits<Allocator>::template rebind_alloc<shared_ptr_impl>
-        allocator_type;
-
-    shared_ptr_impl(T *ptr)
-    : obj(ptr)
-    , ref_count(1)
-    {}
-
-    ~shared_ptr_impl() {
-        using alloc_traits = std::allocator_traits< Allocator >; 
-        // checked delete (https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Checked_delete)
-        checked_delete(obj);
-
-        Allocator allocator;
-        alloc_traits::destroy(allocator, obj);
-        alloc_traits::deallocate(allocator, obj, 1);
-    }
-
-    friend class shared_ptr<T, Allocator>; /*the only user*/
-
-private:
-    T *obj;
-    std::atomic<int> ref_count;
-}; // class shared_ptr_impl
-
-} // namespace detail
-
 
 template<
     typename T,
@@ -75,7 +30,6 @@ public:
 
             scoped_relax(cb_guard);
         }
-            
     }
 
     // Take ownership of obj
@@ -197,7 +151,6 @@ private:
 
     detail::shared_ptr_impl<T, Allocator>* impl;
 };
-
 
 
 template<typename T, typename Allocator = std::allocator<T>, typename ...Args>
