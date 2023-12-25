dae37263d72f6ff15f18ae1f7cc8a0a4547c91b9
vafo <v.safokhujaev@student.inha.uz>
2023-12-25 19:30:07 +0500

    scoped_ptr minor refactor

--- src/util/scoped_ptr.hpp
+++ src/util/scoped_ptr.hpp
@@ -11,14 +11,13 @@ template<
 >
 class scoped_ptr {
 public:
-    scoped_ptr(T* in_ptr)
+    scoped_ptr(T* in_ptr = nullptr)
         : m_ptr(in_ptr)
     { }
 
-    scoped_ptr(const scoped_ptr& other) = delete;
-
     scoped_ptr(scoped_ptr&& other)
-    : m_ptr(other.m_ptr) {
+        : m_ptr(other.m_ptr)
+    {
         other.relax();
     }
 
@@ -27,15 +26,21 @@ public:
 
         checked_delete(m_ptr);
         if(m_ptr != nullptr) {
-            Allocator allocator;
-            alloc_traits::deallocate(allocator, m_ptr, 1);
+            alloc_traits::deallocate(Allocator(), m_ptr, 1);
         }
     }
     
+    scoped_ptr(const scoped_ptr& other) = delete;
     scoped_ptr& operator=(const scoped_ptr& other) = delete;
 
-    void relax() {
+    T* relax() {
+        T* tmp = m_ptr;
         m_ptr = nullptr;
+        return tmp;
+    }
+
+    T* get() {
+        return m_ptr;
     }
 
 private:
